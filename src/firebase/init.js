c87d6b10400c6836c15bbb741fd44bf04e0b1bcf
Nenad Pesic <nenad.pesic32@gmail.com>
2020-01-23 08:43:42 +0100

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,13 +3,13 @@ import firestore from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyA2VCrK-_uYjRVOdLTdNkoqJYQSGp2aF9g",
-  authDomain: "smoothies-6a3d3.firebaseapp.com",
-  databaseURL: "https://smoothies-6a3d3.firebaseio.com",
-  projectId: "smoothies-6a3d3",
-  storageBucket: "smoothies-6a3d3.appspot.com",
-  messagingSenderId: "197999608780",
-  appId: "1:197999608780:web:c032854b01a5b3fb7a9369"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
